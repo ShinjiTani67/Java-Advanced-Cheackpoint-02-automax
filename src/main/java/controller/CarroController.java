a8f797567d6acfcee494e67dcd7ef5590daa1dbd
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2025-04-22 11:37:44 -0300

    update carrocontroller

--- src/main/java/controller/CarroController.java
+++ src/main/java/controller/CarroController.java
@@ -3,8 +3,11 @@ package controller;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.java.Log;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
 import service.CarroService;
 
 @Controller
@@ -14,5 +17,9 @@ import service.CarroService;
 public class CarroController {
 
     private final CarroService service;
-
+    @GetMapping
+    public String listarCarros(Model model) {
+        model.addAttribute("carro", service.getCarro());
+        return "carro/lista";
+    }
 }
