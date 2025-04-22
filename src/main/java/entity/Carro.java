3392c33aae02e938dfb7fcd8b33a0575aa640d0e
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2025-04-22 11:05:17 -0300

    update carro e create carrorepository

--- src/main/java/entity/Carro.java
+++ src/main/java/entity/Carro.java
@@ -1,18 +1,29 @@
 package entity;
 
 
-import jakarta.persistence.Entity;
-import lombok.AllArgsConstructor;
+import jakarta.persistence.*;
 import lombok.Data;
+import lombok.ToString;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.UUID;
 
 @Entity
 @Data
-@AllArgsConstructor
+@ToString
 public class Carro {
 
-    UUID uuid;
-    String modelo;
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID uuid;
+
+    @Column(name="modelo",length = 100, nullable = false)
+    private String modelo;
+
+    @Column(name="preco",length = 100, nullable = false)
+    private double preco;
+
+    @DateTimeFormat
+    private int datavenda;
+
 }
---
+++ src/main/java/repository/CarroRepository.java
@@ -0,0 +1,11 @@
+package repository;
+
+import entity.Carro;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface CarroRepository extends JpaRepository {
+    Optional<Carro> findByUuid(UUID uuid);
+}
