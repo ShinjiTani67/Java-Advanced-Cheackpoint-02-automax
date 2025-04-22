e03fe176b72750ce7071f04665e40c591a3eb3f3
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2025-04-22 11:09:46 -0300

    create carrodto

---
+++ src/main/java/dto/CarroDTO.java
@@ -0,0 +1,27 @@
+package dto;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.UUID;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class CarroDTO {
+
+    private UUID uuid;
+
+    private String modelo;
+
+    private double preco;
+
+    private int datavenda;
+
+}
