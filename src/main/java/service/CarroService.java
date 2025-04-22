ec86a42d0a661e7dfb61f0ce2bf3ec0f43f44919
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2025-04-22 11:50:15 -0300

    update carroservice

--- src/main/java/service/CarroService.java
+++ src/main/java/service/CarroService.java
@@ -1,9 +1,45 @@
 package service;
 
+import dto.CarroDTO;
+import entity.Carro;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import repository.CarroRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
 public class CarroService {
+    private final CarroRepository repository;
+
+    private CarroDTO convertToDTO(Carro carro) {
+        CarroDTO dto = new CarroDTO();
+        dto.setUuid(carro.getUuid());
+        dto.setModelo(carro.getModelo());
+        dto.setDatavenda(carro.getDatavenda());
+        dto.setPreco(carro.getPreco());
+        return dto;
+    }
+
+    private Carro convertToEntity(CarroDTO dto){
+        Carro carro = new Carro();
+        carro.setUuid(dto.getUuid());
+        carro.setModelo(dto.getModelo());
+        return carro;
+    }
+
+    public CarroDTO save(CarroDTO carroDTO){
+        Carro carro = convertToEntity(carroDTO);
+        carro = (Carro) repository.save(carro);
+        return convertToDTO(carro);
+    }
+
+    public List<CarroDTO> getCarro(){
+        List<CarroDTO> carro = repository.findAll().stream()
+                .map(this::convertToDTO)
+                .collect(Collectors.toList());
+        return carro;
+    }
 }
