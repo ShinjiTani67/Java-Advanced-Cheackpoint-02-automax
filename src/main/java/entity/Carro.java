53660133446cadbd1af278e550478e783f34887b
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2025-04-22 10:58:17 -0300

    update carro

--- src/main/java/entity/Carro.java
+++ src/main/java/entity/Carro.java
@@ -1,4 +1,18 @@
 package entity;
 
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Entity
+@Data
+@AllArgsConstructor
 public class Carro {
+
+    UUID uuid;
+    String modelo;
+    
 }
