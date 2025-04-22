7aef9e60058f048ac06a25f01fa2105c0e8eb028
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2025-04-22 10:29:32 -0300

    git innit

---
+++ src/main/java/fiap/com/br/automax/AutomaxApplication.java
@@ -0,0 +1,13 @@
+package fiap.com.br.automax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomaxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fiap/com/br/automax/AutomaxApplicationTests.java
@@ -0,0 +1,13 @@
+package fiap.com.br.automax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomaxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
