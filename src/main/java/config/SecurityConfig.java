583a27e287302d214ffcf9c0dece99e718fa427e
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2025-04-24 16:48:58 -0300

    update securityconfig

--- src/main/java/config/SecurityConfig.java
+++ src/main/java/config/SecurityConfig.java
@@ -16,7 +16,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
+        https
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/vendedor/novo", "/vendedor/salvar", "/vendedor/editar/**", "/vendedor/deletar/**").hasRole("ADMIN")
                 .requestMatchers("/vendedor/**").hasAnyRole("ADMIN", "USER")
