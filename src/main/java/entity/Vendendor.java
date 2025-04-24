f097c4fd233192f7b4e5642b9521df5fb79787ca
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2025-04-24 16:22:05 -0300

    create vendendor

---
+++ src/main/java/entity/Vendendor.java
@@ -0,0 +1,30 @@
+package entity;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.ToString;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Data
+@ToString
+public class Vendendor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name="modelo",length = 100, nullable = false)
+    private String nome;
+
+    @Column(name="preco",length = 100, nullable = false)
+    private double salario;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate dataentrada;
+
+}
