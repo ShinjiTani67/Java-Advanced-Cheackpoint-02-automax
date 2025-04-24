a2973c2a283083be201e52f5ea3447c9f05c85a6
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2025-04-24 16:29:43 -0300

    create vendedorcontroller

---
+++ src/main/java/controller/VendedorController.java
@@ -0,0 +1,49 @@
+package controller;
+
+import dto.VendedorDTO;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.ui.Model;
+import service.VendedorService;
+
+@Controller
+@AllArgsConstructor
+@Slf4j
+@RequestMapping("/template.vendedor")
+public class VendedorController {
+
+    private final VendedorService service;
+
+    @GetMapping
+    public String listarVendedor(Model model) {
+        model.addAttribute("template.vendedor", service.getVendedor());
+        return "template.vendedor/lista";
+    }
+
+    @GetMapping("/novo")
+    public String novoVendedor(Model model){
+        model.addAttribute("template.vendedor", new VendedorDTO());
+        return "template.vendedor/formulario";
+    }
+
+    @PostMapping("/salvar")
+    public String salvarVendedor(
+            @Valid @ModelAttribute("template.vendedor") VendedorDTO vendedorDTO,
+            BindingResult bindingResult,
+            Model model
+    ) {
+        if (bindingResult.hasErrors()) {
+            bindingResult.getAllErrors().forEach(e -> log.info(e.toString()));
+            model.addAttribute("template.vendedor", vendedorDTO);
+            return "template.vendedor/formulario";
+        }
+        service.save(vendedorDTO);
+        return "redirect:/vendedor";
+    }
+
+    @GetMapping("/editar/{id}")
+    public String deletar(@PathVariable Long id){
