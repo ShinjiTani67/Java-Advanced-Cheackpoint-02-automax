077908f564996103e7044b606539308c8fad0e89
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2025-04-24 09:25:25 -0300

    update

--- src/main/java/controller/CarroController.java
+++ src/main/java/controller/CarroController.java
@@ -1,12 +1,14 @@
 package controller;
 
 
+import dto.CarroDTO;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.extern.java.Log;
 import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
 import service.CarroService;
 
@@ -17,9 +19,36 @@ import service.CarroService;
 public class CarroController {
 
     private final CarroService service;
+
     @GetMapping
-    public String listarCarros(Model model) {
+    public String listarCarro(Model model) {
         model.addAttribute("carro", service.getCarro());
         return "carro/lista";
+    }
+
+    @GetMapping("/novo")
+    public String novoCarro(Model model){
+        model.addAttribute("carro", new CarroDTO());
+        return "carro/formulario";
+    }
+    @PostMapping("/salvar")
+    public String salvarCarro(
+            @Valid @ModelAttribute("carro") CarroDTO carroDTO,
+            BindingResult bindingResult,
+            Model model
+    ) {
+        if (bindingResult.hasErrors()) {
+            bindingResult.getAllErrors().forEach(e -> log.info(e.toString()));
+            model.addAttribute("carro", carroDTO);
+            return "carro/formulario";
+        }
+        service.save(carroDTO);
+        return "redirect:/carro";
+    }
+
+    @GetMapping("/editar/{id}")
+    public String deletar(@PathVariable Long id){
+        service.deletarPorId(id);
+        return "redirect:/carro";
     }
 }
